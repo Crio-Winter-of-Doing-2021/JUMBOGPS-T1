393881a07894df00da44e5952d3c347f2fd91f18
RajatSvN <rajat901cx@gmail.com>
2021-04-05 21:31:42 +0530

    Fix Typo

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use(uiRouter)
 app.use(userRouter)
 app.use(assetRouter)
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
     console.log(`Started Server on port ${port}`)
